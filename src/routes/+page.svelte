1e7599134a1cf4586a67b883dcdf85fa739ee837
fcrozatier <frederic.crozatier@protonmail.com>
2023-01-04 22:14:21 +0100

    wording

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1>Modify me and see HRM in action</h1>
+<h1>Modify me and see hot module reloading in action</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
